7d623046bbfc07010e3e0ed1373e3e1b23e01327
Sambhaji Kolate <kolate.sambhaji@gmail.com>
2015-08-11 14:24:25 +0530

    updated script to calculate Supplier Inv Number and removed old report file

--- sf_custom_changes/sf_custom_changes/report/custom_bank_reconciliation_statement/custom_bank_reconciliation_statement.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2013, Systematrix and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Custom Bank Reconciliation Statement"] = {
-	"filters": [
-
-	]
-}
