38cd87c4bb63ba6c953e4e40d11ecd06770c8fca
Omkar Ghatpande <omkar.ghatpande@gmail.com>
2015-11-02 18:37:33 +0530

    Update sales_invoice_custom_report.py
    Added Posting Date to Sales Invoice Custom Report

--- sf_custom_changes/sf_custom_changes/report/sales_invoice_custom_report/sales_invoice_custom_report.py
+++ sf_custom_changes/sf_custom_changes/report/sales_invoice_custom_report/sales_invoice_custom_report.py
@@ -14,7 +14,7 @@ def execute(filters=None):
 	return columns, data
 	
 def get_columns():
-	return [_("ID") + ":Link/Sales Invoice:140", _("Customer Name") + ":Link/Customer:140", _("Customer PO NO.") +  ":Data:100",
+	return [_("ID") + ":Link/Sales Invoice:140", _("Date") +  ":Date:140", _("Customer Name") + ":Link/Customer:140", _("Customer PO NO.") +  ":Data:100",
                 _("Grand Total") +  ":Currency:100", _("Due Date") +  ":Date:100", _("Outstanding Amount") +  ":Currency:140",
                 _("Credit Days") +  ":Data:100"
 	]
@@ -29,5 +29,5 @@ def get_conditions(filters):
 	
 def get_entries(filters):
 	conditions = get_conditions(filters)
-	entries =  frappe.db.sql("""select name, customer_name, po_no, grand_total_export, due_date, outstanding_amount, credit_days from `tabSales Invoice` where  %s and docstatus!=2 order by name DESC""" % conditions, filters, as_list=1)
+	entries =  frappe.db.sql("""select name, posting_date, customer_name, po_no, grand_total_export, due_date, outstanding_amount, credit_days from `tabSales Invoice` where  %s and docstatus!=2 order by name DESC""" % conditions, filters, as_list=1)
 	return entries
