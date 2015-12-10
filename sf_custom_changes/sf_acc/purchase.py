df3cbf2d5f464e924f73138f8959d796b061d89b
Omkar Ghatpande <omkar.ghatpande@gmail.com>
2015-12-10 19:24:40 +0530

    Update purchase.py
    Corrected a bug from the last commit

--- sf_custom_changes/sf_acc/purchase.py
+++ sf_custom_changes/sf_acc/purchase.py
@@ -28,7 +28,7 @@ def validate_mandatory(self):
 def validate_bill_no(self, method):
 		if self.bill_no:
 			# validate bill no is unique
-			bill_list = frappe.db.sql("""select name, supplier from `tabPurchase Invoice` where bill_no=%s and docstatus =1 and is_recurring='0'""",
+			bill_list = frappe.db.sql("""select name, supplier from `tabPurchase Invoice` where bill_no=%s and supplier=%s and docstatus=1 and is_recurring='0'""",
 				self.bill_no, self.supplier)
 			if len(bill_list) > 0:
 				items = [e[0] for e in bill_list if e[0]!=self.name]
